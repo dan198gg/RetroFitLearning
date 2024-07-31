a23e56e4312e8d03eca49206c41b5d788c125f94
dan198gg <danzelgoncharenko@gmail.com>
2024-07-31 17:49:44 +0300

    TextFieldsIsAdded

--- app/src/main/java/com/example/retrofitlearning/MainActivity.kt
+++ app/src/main/java/com/example/retrofitlearning/MainActivity.kt
@@ -5,16 +5,27 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.retrofitlearning.dataproduct.ProductApi
 import com.example.retrofitlearning.datauser.DataPostUser
 import com.example.retrofitlearning.ui.theme.RetroFitLearningTheme
@@ -31,33 +42,62 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
 class MainActivity : ComponentActivity() {
+    var login by
+        mutableStateOf("")
+
+    var password by
+        mutableStateOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val interceptor=HttpLoggingInterceptor()
-        interceptor.level=HttpLoggingInterceptor.Level.BODY
-        val client= OkHttpClient.Builder()
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder()
             .addInterceptor(interceptor)
             .build()
-        val retrofit=Retrofit.Builder()
+        val retrofit = Retrofit.Builder()
             .baseUrl("https://dummyjson.com")
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
-        val productApi=retrofit.create(ProductApi::class.java)
+        val productApi = retrofit.create(ProductApi::class.java)
 
         setContent {
-            val corScope= rememberCoroutineScope()
-            LaunchedEffect(key1 = "") {
-            corScope.launch {
-                    val user=productApi.auth(DataPostUser(30,"emilyspass","emilys"))
-                    Log.i("INFOUSER",user.email)
+            val corScope = rememberCoroutineScope()
+            Column(modifier = Modifier.padding(0.dp,50.dp)) {
+                TextFieldsForUsers()
+                Button(onClick = {
+                    corScope.launch {
+                        val user = productApi.auth(DataPostUser(30, password, login))
+                        Log.i("INFOUSER", user.email)
+                    }
+                }) {
+                    Text(text = "Войти")
                 }
             }
         }
     }
-}
 
+
+    @Composable
+    fun TextFieldsForUsers() {
+        Column {
+            TextField(
+                value = login,
+                onValueChange = { login = it },
+                placeholder = { Text(text = "Введите логин...") }
+            )
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                placeholder = { Text(text = "Введите пароль...") },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            )
+        }
+    }
+}
 
 //val product1 = productApi.getProduct(8)
 //                Log.i("INFOPRODUCT",product1.brand)
